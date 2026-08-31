273e3d42b528aec3bdfaa78772cc98cc0200b234
Ahmedbadawy123 <ahmed.mohamedjohar@gmail.com>
2026-08-31 12:07:29 +0300

    Question01

--- AssignmentADVC01/Program.cs
+++ AssignmentADVC01/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1: What is a generic class? Why use generics?
+
+            // Is A Class That Allows You To Write Classes, Interfaces and Methods With Type Parameters , While The Actual Type Is Specified When The Code Is Used,Not When It Is Written.
+            // Because It Is Type Safe, Reusable, Better, Cleaner Code.
+
+            #endregion
+
         }
     }
 }
